115bd344eb13a1f3b93a47711c51cce4c7aacf49
luke-who <33724568+luke-who@users.noreply.github.com>
2025-03-01 22:05:36 +0000

    Update date

--- ~/airflow/dags/dag_test_dependencies.py
+++ ~/airflow/dags/dag_test_dependencies.py
@@ -1,11 +1,7 @@
-from airflow.models import DAG
-from datetime import datetime
-from datetime import timedelta
-from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.models import Variable
-
-weather_dir = Variable.get("weather_dir")
-# weather_dir = "~/Library/CloudStorage/GoogleDrive-luke.z0423@gmail.com/My Drive/Colab Notebooks/AiCore/Courses/AWS Data Engineering Services/airflow-weather-data"
+from datetime import datetime, timedelta
 
 default_args = {
     "owner": "Ivan",
@@ -14,12 +10,11 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "start_date": datetime(
-        2025, 3, 1
-    ),  # If you set a datetime previous to the curernt date, it will try to backfill
+    "start_date": datetime(2023, 1, 1),  # Changed to past date for immediate execution
     "retry_delay": timedelta(minutes=5),
     "end_date": datetime(2030, 1, 1),
 }
+
 with DAG(
     dag_id="test_dag_dependencies",
     default_args=default_args,
@@ -27,28 +22,33 @@ with DAG(
     catchup=False,
     tags=["test"],
 ) as dag:
-    # Define the tasks. Here we are going to define only one bash operator
+
+    # Create directory if not exists and write date
     date_task = BashOperator(
         task_id="write_date",
-        bash_command="cd weather_dir && date >> date.txt",
-        dag=dag,
-    )
-    add_task = BashOperator(
-        task_id="add_files",
-        bash_command="cd weather_dir && git add .",
-        dag=dag,
-    )
-    commit_task = BashOperator(
-        task_id="commit_files",
-        bash_command='cd weather_dir && git commit -m "Update date"',
-        dag=dag,
-    )
-    push_task = BashOperator(
-        task_id="push_files",
-        bash_command="cd weather_dir && git push",
-        dag=dag,
+        bash_command='mkdir -p "{{ var.value.weather_dir }}" && '
+        'cd "{{ var.value.weather_dir }}" && '
+        "date >> date.txt",
     )
 
-    date_task >> add_task >> commit_task
-    add_task >> push_task
-    commit_task >> push_task
+    # Git add command
+    add_task = BashOperator(
+        task_id="add_files",
+        bash_command='cd "{{ var.value.weather_dir }}" && ' "git add .",
+    )
+
+    # Git commit command
+    commit_task = BashOperator(
+        task_id="commit_files",
+        bash_command='cd "{{ var.value.weather_dir }}" && '
+        'git commit -m "Update date"',
+    )
+
+    # Git push command
+    push_task = BashOperator(
+        task_id="push_files",
+        bash_command='cd "{{ var.value.weather_dir }}" && ' "git push",
+    )
+
+    # Simplified task dependencies
+    date_task >> add_task >> commit_task >> push_task
